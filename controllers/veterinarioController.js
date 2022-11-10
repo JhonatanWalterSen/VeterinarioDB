761d19fb0395d0669969b8e5a8bf668710f61140
Jhonatan Ariza <arizaj@LIM915010.local>
2022-11-10 16:24:20 -0500

    endpoint registrar y mostrar, create routes and controllers

---
+++ controllers/veterinarioController.js
@@ -0,0 +1,11 @@
+const registrar = (req,res)=>{
+    res.json({msg:"Registrando Usuario"});
+}
+const perfil = (req,res)=>{
+    res.json({msg:"Mostrando perfil"});
+}
+
+export {
+    registrar,
+    perfil
+}
